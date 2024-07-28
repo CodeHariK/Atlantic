0f103969e8d5ee3683383c0c40df66c3a2a742fb
Hari Krishnan <codeharik@gmail.com>
2024-07-28 23:38:08 +0530

    Skitty UI

--- skitty/astro.config.mjs
+++ skitty/astro.config.mjs
@@ -1,14 +1,26 @@
 
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
 import purgecss from 'astro-purgecss';
+import solid from '@astrojs/solid-js';
+
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
+    site: 'https://atlantic.shark.run',
     server: { port: 8080 },
     integrations: [
-        svelte(),
+        solid(),
+
         tailwind(),
-        purgecss()
+        purgecss(),
+
+        sitemap(),
+        robotsTxt(),
     ],
+
+    devToolbar: {
+        enabled: false
+    }
 });
--- skitty/tailwind.config.mjs
+++ skitty/tailwind.config.mjs
@@ -2,12 +2,9 @@
 module.exports = {
     content: [
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-        './node_modules/flowbite-svelte/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-        'node_modules/preline/dist/*.js',
     ],
     plugins: [
         require('flowbite/plugin'),
-        require('preline/plugin')
     ],
 
     darkMode: 'class'
